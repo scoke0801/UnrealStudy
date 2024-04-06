a504fe1a57c2ed314f25432ad751f23e1d7f96ef
Hyun0801 <scoke0801@gmail.com>
2024-04-06 20:54:58 +0900

    ~2ch

---
+++ ArenaBattle/Source/ArenaBattle/Fountain.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/PointLightComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Fountain.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AFountain : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFountain();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// 가비지 콜렉터를 통한 메모리 해제를 자동으로 언리얼 엔진이 해줌
+	// 단, UPROPERTY() 매크로를 사용해야 함
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Body;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent* Light;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* Splash;
+
+	UPROPERTY(EditAnywhere, Category = ID)
+	int32 ID;
+};
