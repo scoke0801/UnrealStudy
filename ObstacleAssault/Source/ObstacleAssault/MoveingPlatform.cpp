8eba9b2844a866ec22c54ac4ce5f09ea02db3edb
Hyun0801 <scoke0801@gmail.com>
2024-09-07 22:08:39 +0900

    Obstacle project add

---
+++ ObstacleAssault/Source/ObstacleAssault/MoveingPlatform.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MoveingPlatform.h"
+
+// Sets default values
+AMoveingPlatform::AMoveingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMoveingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetActorLocation(_vectorVar);
+}
+
+// Called every frame
+void AMoveingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	SetActorLocation(_vectorVar);
+    _vectorVar.Y += 1;
+}
+
