c17e75ec20e03a15e4c769eb728cbdd1bfc4bf20
Hyun0801 <scoke0801@gmail.com>
2024-09-17 17:50:04 +0900

    Sentry 플러그인 테스트

--- PluginTest_5_4_4/Source/PluginTest_5_4_4/PluginTest_5_4_4.Build.cs
+++ PluginTest_5_4_4/Source/PluginTest_5_4_4/PluginTest_5_4_4.Build.cs
@@ -8,6 +8,6 @@ public class PluginTest_5_4_4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Sentry" });
 	}
 }
