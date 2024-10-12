803b4a2b07981c0ddc335d5800e5e716ed158a2c
Hyun0801 <scoke0801@gmail.com>
2024-10-12 15:01:51 +0900

    Widget test

---
+++ UITest/Source/UITest/Private/Management/ManagerBase.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Management/ManagerBase.h"
+
+// Add default functionality here for any IManagerBase functions that are not pure virtual.
---
+++ UITest/Source/UITest/Public/UI/Popup/UIPopupBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/GameWidgetBase.h"
+#include "UIPopupBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UITEST_API UUIPopupBase : public UGameWidgetBase
+{
+	GENERATED_BODY()
+	
+};
